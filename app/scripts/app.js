3eb0e2f52ae7252497076b874632dc2fe075a7fb
AdreeUA <000scorpio000@gmail.com>
2016-04-28 21:40:47 +0300

    inition commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,18 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+// import 'slick-carousel';
 
 $(() => {
 	svg4everybody();
 });
+
+(function () {
+	const burger = document.getElementById('burger-button');
+	const nav = document.getElementById('navigation');
+
+	burger.addEventListener('click', (e) => {
+		e.preventDefault();
+		nav.classList.toggle('nav_open');
+		burger.classList.toggle('main-menu__burger_open');
+	});
+})();
